ea550337c562469bb8333f9277aea5344957548a
Chammmppp <champ.thichanon@gmail.com>
2024-06-17 16:40:25 +0700

    slug

--- store/views.py
+++ store/views.py
@@ -11,13 +11,12 @@ def store(request, category_slug=None):
     
     
     if category_slug != None:
-        categories = get_object_or_404(slug=category_slug)
-        product = Product.objects.filter(category = categories, is_available =True)
+        categories = get_object_or_404(Category,slug=category_slug)
+        products = Product.objects.filter(category = categories, is_available =True)
         product_count = products.count()
     else:
         products= Product.objects.all().filter(is_available=True)
         product_count=products.count()
-    products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
+        
     context = {"products": products, "product_count": product_count}
     return render(request, "store/store.html", context)
